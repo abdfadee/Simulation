ce0686e1b2f081244940c4cc9baae23d4749a4bf
abdfadee <abdfadee2002@gmail.com>
2025-05-16 18:19:49 +0300

    'shapecast'

--- main.js
+++ main.js
@@ -7,165 +7,80 @@ import "./lib/renderer/Skybox.js";
 import physicsEngine from "./lib/physics/PhysicsEngine.js";
 import RigidBody from "./lib/physics/RigidBody.js";
 
-import {} from "./lib/physics/Collision.js";
-import { MeshBVH , MeshBVHHelper , SAH } from 'three-mesh-bvh';
-import * as BufferGeometryUtils from 'three/addons/utils/BufferGeometryUtils.js';
+import {getContact} from "./lib/physics/Collision.js";
 
 
 
-function generateCollisionMesh (mesh) {
-    const geometries = [];
-
-    mesh.traverse(function (child) {
-    if (child.isMesh) {
-        child.updateMatrixWorld(true);
-        const clonedGeo = child.geometry.clone();
-        clonedGeo.applyMatrix4(child.matrixWorld);
-        clonedGeo.deleteAttribute('tangent');
-        geometries.push(clonedGeo);
-    }
-    });
-
-    const mergedGeometry = BufferGeometryUtils.mergeGeometries(geometries, true);
-    mergedGeometry.boundsTree = new MeshBVH(mergedGeometry, {
-        strategy: SAH,
-        maxLeafTris: 1,
-    });
-
-    const mergedMesh = new THREE.Mesh(mergedGeometry, new THREE.MeshBasicMaterial({ wireframe: true,visible:false }));
-    mesh.collider = mergedMesh;
-    mesh.add(mesh.collider);
-
-    const helper = new MeshBVHHelper(mergedMesh,10);
-    helper.children[0].material.color.set(0xff0000);
-    helper.children[0].material.transparent = true
-    //scene.add(helper);
-}
 
 
-function getGeometryToBVH (meshA , meshB) {
-    const matrixToB = new THREE.Matrix4()
-        .copy(meshB.matrixWorld)
-        .invert()
-        .multiply(meshA.matrixWorld);
-    return matrixToB;
-}
 
 
 async function main () {
     camera.position.set(0,8,8);
     
 
-
     /* Rendering */
-    
-    // Ground
-    /*
-    const floorGeometry = new THREE.PlaneGeometry(100,100,1000,1000);
-
-    const floorAlbedo = textureLoader.load("./assets/texture/PavingStones115/PavingStones115B_1K-JPG_Color.jpg");
-    const floorNormal = textureLoader.load("./assets/texture/PavingStones115/PavingStones115B_1K-JPG_NormalGL.jpg");
-    const floorDisplacement = textureLoader.load("./assets/texture/PavingStones115/PavingStones115B_1K-JPG_Displacement.jpg");
-    const floorRoughness = textureLoader.load("./assets/texture/PavingStones115/PavingStones115B_1K-JPG_Roughness.jpg");
-    const floorAo = textureLoader.load("./assets/texture/PavingStones115/PavingStones115B_1K-JPG_AmbientOcclusion.jpg");
-    floorAlbedo.colorSpace = THREE.SRGBColorSpace;
-
-    const textures = [floorAlbedo,floorNormal,floorDisplacement,floorRoughness,floorAo];
-    textures.forEach((e) => {
-    e.wrapS = THREE.RepeatWrapping;
-    e.wrapT = THREE.RepeatWrapping; 
-    e.repeat.set(30, 30);
-    });
-
-    const floorMaterial = new THREE.MeshPhysicalMaterial({
-    map: floorAlbedo,
-    normalMap: floorNormal,
-    displacementMap: floorDisplacement,
-    displacementScale: 0.2,
-    roughnessMap: floorRoughness,
-    aoMap: floorAo
-    });
-
-    const floor = new THREE.Mesh(floorGeometry,floorMaterial);
-    floor.rotateX(MathUtils.degToRad(-90.0));
-    floor.receiveShadow = true;
-    scene.add(floor);
-    */
-
-
-
     const terrainModel = await modelLoader.loadAsync('assets/model/terrain/scene.gltf');
-    const terrain = terrainModel.scene;
-    terrain.scale.set(1,1,1);
-    //terrain.rotateY(MathUtils.degToRad(90));
-    //terrain.rotateX(MathUtils.degToRad(15));
-    terrain.position.set(0,0.4,0);
-    terrain.traverse(function (child) {
+    const terrain3D = terrainModel.scene;
+    terrain3D.scale.set(1,1,1);
+    terrain3D.position.set(0,0.4,0);
+    terrain3D.traverse(function (child) {
     if (child.isMesh) {
         child.castShadow = true
         child.receiveShadow = true
         child.side = THREE.BackSide;
     }
     });
-    scene.add(terrain);
-    generateCollisionMesh(terrain);
-
-
+    terrain3D.generateCollisionMesh();
 
 
     const shellModel = await modelLoader.loadAsync('assets/model/cannonball/scene.gltf');
-    const shell = shellModel.scene;
-    shell.scale.set(3,3,3);
-    shell.position.set(0,5,0);
-    shell.traverse(function (child) {
+    const shell3D = shellModel.scene;
+    shell3D.scale.set(3,3,3);
+    shell3D.position.set(0,5,0);
+    shell3D.traverse(function (child) {
     if (child.isMesh) {
         child.castShadow = true
         child.receiveShadow = true
         child.side = THREE.BackSide;
     }
     });
-    scene.add(shell);
-    generateCollisionMesh(shell);
+    shell3D.generateCollisionMesh();
+
 
 
 
     /* Physics */
-    const pshell = new RigidBody(shell,1.0,0.5,0.4);
-    physicsEngine.addBody(pshell);
+    const terrain = new RigidBody(terrain3D,0.0,0.5,0.4);
+    physicsEngine.addBody(terrain);
+
+    const shell = new RigidBody(shell3D,1.0,0.5,0.4);
+    physicsEngine.addBody(shell);
 
 
 
 
     const clock = new THREE.Clock();
     function animate(time) {
-        const matrixToB = getGeometryToBVH(shell.collider,terrain.collider);
+        //camera.lookAt(shell.position);
+        //camera.updateProjectionMatrix();
+        
+        shell.addForce(new THREE.Vector3(5, 0, 0));
 
-        // Use boundsTree from meshA to check against meshB's geometry
-        const intersects = terrain.collider.geometry.boundsTree.intersectsGeometry(
-            shell.collider.geometry,
-            matrixToB
-        );
+        const delta = clock.getDelta();
+        physicsEngine.update(delta/10);
 
+        /*
+        const intersects = shell.intersects(terrain);
         if (intersects) {
         console.log('Collision detected!');
         } else {
         console.log('No collision.');
         }
+        */
 
-
-        //shell.rotateY(MathUtils.degToRad(1));
-
-        //terrain.rotateZ(MathUtils.degToRad(1));
-        //terrain.updateMatrixWorld(true);
-
-        //terrain.translateY(-0.001);
-
-        //camera.lookAt(shell.position);
-        //camera.updateProjectionMatrix();
-
-        pshell.addForce(new THREE.Vector3(5, 0, 0));
-        const delta = clock.getDelta();
-        physicsEngine.update(delta/10);
+        const contact = getContact(shell,terrain);
+        console.log(contact);
 
         renderer.render( scene, camera );
     }
